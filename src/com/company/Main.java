1931de76cf398ad00b824277fa34d0f0cddcd10f
jyebe <jyebe9034@gmail.com>
2021-06-18 15:41:33 +0900

    update Main.java and add comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,23 +2,20 @@ package com.company;
 
 import java.util.Scanner;
 
+/**
+ * Geometry: area of a triangle
+ * created: 2021. 06. 18.
+ */
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        float x1 = scanner.nextFloat();
-//        float y1 = scanner.nextFloat();
-//        float x2 = scanner.nextFloat();
-//        float y2 = scanner.nextFloat();
-//        float x3 = scanner.nextFloat();
-//        float y3 = scanner.nextFloat();
-
-        double x1 = 1.5;
-        double y1 = -3.4;
-        double x2 = 4.6;
-        double y2 = 5;
-        double x3 = 9.5;
-        double y3 = -3.4;
+        float x1 = scanner.nextFloat();
+        float y1 = scanner.nextFloat();
+        float x2 = scanner.nextFloat();
+        float y2 = scanner.nextFloat();
+        float x3 = scanner.nextFloat();
+        float y3 = scanner.nextFloat();
 
         double side1 = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         double side2 = Math.sqrt((y3 - y2) * (y3 - y2) + (x3 - x2) * (x3 - x2));
